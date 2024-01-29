f65e18294f47344eaa1adcd69f159d19780fe8ab
kyh012412 <kyh0124.12@gmail.com>
2024-01-29 10:03:03 +0900

    3배포끝

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>Hello, Nextjs! <br/>
-    !!!!!!!!!!!!</>
+    !!!!!!!!!!!!<br/>
+    @@@@@@@@@@@@@</>
   );
 }
 //여기서 리턴한 값을 layout.js에 있는 {children}자리에서 사용
