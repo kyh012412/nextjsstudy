acf049cfb66c03c5609e180e1be90d55acc5127d
kyh012412 <kyh0124.12@gmail.com>
2024-01-29 10:11:45 +0900

    4뼈대만들기끝

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>Hello, Nextjs! <br/>
-    !!!!!!!!!!!!<br/>
-    @@@@@@@@@@@@@</>
+    <>
+      <h2>Welcome</h2>
+      Hello, WEB!
+    </>
   );
 }
 //여기서 리턴한 값을 layout.js에 있는 {children}자리에서 사용
